698f8bcfe816b735597e0e179675a8f079e72af7
NicolayNelson <59945806+NicolayNelson@users.noreply.github.com>
2020-03-09 17:38:40 -0700

    Added files with elements labled

---
+++ script.js
@@ -0,0 +1,25 @@
+let container = document.getElementById("")
+let calculator = document.getElementById("calculator");
+
+let display = document.getElementById("display");
+
+let zero = document.getElementById("button-0");
+let one = document.getElementById("button-1");
+let two = document.getElementById("button-2");
+let three = document.getElementById("button-3");
+let four = document.getElementById("button-4");
+let five = document.getElementById("button-5");
+let six = document.getElementById("button-6");
+let seven = document.getElementById("button-7");
+let eight = document.getElementById("button-8");
+let nine = document.getElementById("button-9");
+let plus = document.getElementById("button-plus");
+let minus = document.getElementById("button-minus");
+let multiply = document.getElementById("button-multiply");
+let divide = document.getElementById("button-divide");
+let equals = document.getElementById("button-equals");
+let clear= document.getElementById("button-clear");
+
+function one () {
+    document
+}
